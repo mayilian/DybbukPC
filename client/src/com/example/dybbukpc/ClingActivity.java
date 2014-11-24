c999c67a1a4784e15b8b0ebdc968429e7cd64c57
mdavit <davidmailian@gmail.com>
2014-11-24 22:37:44 +0400

    changed socket connection with asynktask

--- client/src/com/example/dybbukpc/ClingActivity.java
+++ client/src/com/example/dybbukpc/ClingActivity.java
@@ -13,68 +13,79 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 public class ClingActivity extends Activity {
 
 	EditText textOut;
 	TextView textIn;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_mute);
 
-		textOut = (EditText)findViewById(R.id.textout);
-	    Button buttonSend = (Button)findViewById(R.id.send);
-	    textIn = (TextView)findViewById(R.id.textin);
-	    buttonSend.setOnClickListener(buttonSendOnClickListener);
+		textOut = (EditText) findViewById(R.id.textout);
+		Button buttonSend = (Button) findViewById(R.id.send);
+		textIn = (TextView) findViewById(R.id.textin);
+		buttonSend.setOnClickListener(buttonSendOnClickListener);
 	}
-	
 
 	Button.OnClickListener buttonSendOnClickListener = new Button.OnClickListener() {
 		@Override
 		public void onClick(View arg0) {
-			Thread thread = new Thread(new Runnable() {
-				@Override
-				public void run() {
-					// TODO Auto-generated method stub
-					Socket socket = null;
-					DataOutputStream dataOutputStream = null;
-					OutputStream out = null;
-					try {
-						socket = new Socket(your IP, 4445);
-						out = socket.getOutputStream();
-						//dataOutputStream = new DataOutputStream(socket.getOutputStream());
-						out.write(textOut.getText().toString().getBytes());
-					} catch (UnknownHostException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					} finally {
-						if (socket != null) {
-							try {
-								socket.close();
-							} catch (IOException e) {
-								// TODO Auto-generated catch block
-								e.printStackTrace();
-							}
-						}
-
-						if (dataOutputStream != null) {
-							try {
-								dataOutputStream.close();
-							} catch (IOException e) {
-								// TODO Auto-generated catch block
-								e.printStackTrace();
-							}
-						}
-					}
-				}
-			});
-			thread.start();
+			new ClientTask("100.82.141.4", 4445).execute();
 		}
 	};
+
+	public class ClientTask extends AsyncTask<Void, Void, Void> {
+
+		String IP;
+		int port;
+
+		ClientTask(String ip, int port) {
+			this.IP = ip;
+			this.port = port;
+		}
+
+		@Override
+		protected Void doInBackground(Void... arg0) {
+			Socket socket = null;
+			DataOutputStream dataOutputStream = null;
+			OutputStream out = null;
+			try {
+				socket = new Socket(IP, port);
+				out = socket.getOutputStream();
+				out.write(textOut.getText().toString().getBytes());
+			} catch (UnknownHostException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			} finally {
+				if (null != socket) {
+					try {
+						socket.close();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+
+				if (null != dataOutputStream) {
+					try {
+						dataOutputStream.close();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+			return null;
+		}
+
+		@Override
+		protected void onPostExecute(Void result) {
+			super.onPostExecute(result);
+		}
+
+	}
 }
